2f20b42ed820231f856497faaf8ce16f86555e4a
Egor <znogoud3@gmail.com>
2019-12-15 10:52:52 +0300

    refactor: as much as possible

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
-
 let lat;
 let lon;
+let descriptionReserve;
+let dataReserve;
+let humidityReserve;
+let windReserve;
+let degreesValue = 'c'; //celsius or fahrenheit
 
 
 
@@ -9,16 +13,13 @@ function getLocation() {
     if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(showPosition);
     } else {
-        console.log("Geolocation is not supported by this browser.");
+        alert("Geolocation is not supported by this browser.");
     }
 }
 
 function showPosition(position) {
-
     lat = position.coords.latitude;
     lon = position.coords.longitude;
-    console.log(lon, lat);
-
     //=============================================MAP===================================================
     const issIcon = L.icon({
         iconUrl: 'marker.png',
@@ -27,7 +28,7 @@ function showPosition(position) {
     });
 
     const mymap = L.map('mapid').setView([lat, lon], 13);
-    const marker = L.marker([0, 0], {icon: issIcon}).addTo(mymap);//no need maybe
+    const marker = L.marker([0, 0], { icon: issIcon }).addTo(mymap);
     const attribution = '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors';
 
     const tileUrl = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
@@ -43,7 +44,7 @@ function showPosition(position) {
     document.getElementById('lon').textContent = lon;
 }
 
-//=====================================================================
+//======================================================================
 
 getLocation();
 //=============================================BACKGROUND===================================================
@@ -52,13 +53,13 @@ function renderItem(state = "weather") {
         document.getElementsByClassName('main-container')[0].style.backgroundImage = `url(${response.url})`;
     })
 }
-//======================================================================
+//=======================================================================
 
 
 
 //=============================================WEATHER===================================================
 function weatherBalloon(cityID) {
-    var key = '29a81f0e66d7fba4e113b2cf97ccb612';
+    let key = '29a81f0e66d7fba4e113b2cf97ccb612';
     fetch('https://api.openweathermap.org/data/2.5/weather?id=' + cityID + '&appid=' + key)
         .then(function (resp) { return resp.json() }) // Convert data to json
         .then(function (data) {
@@ -69,33 +70,20 @@ function weatherBalloon(cityID) {
         });
 }
 
-let descriptionReserve;
-let dataReserve;
-let humidityReserve;
-let windReserve;
-function drawWeather(d) { 
+function drawWeather(d) {
     dataReserve = d;
-    var celsius = Math.round(parseFloat(d.main.temp) - 273.15);
-    var fahrenheit = Math.round(((parseFloat(d.main.temp) - 273.15) * 1.8) + 32);
-
-
-
-
-    var icon = d.weather[0].icon;
-    var humidity = d.main.humidity;
+    let celsius = Math.round(parseFloat(d.main.temp) - 273.15);
+    let icon = d.weather[0].icon;
+    let humidity = d.main.humidity;
     humidityReserve = humidity;
-    var wind = d.wind.speed;
+    let wind = d.wind.speed;
     windReserve = wind;
-    var description = d.weather[0].description;
+    let description = d.weather[0].description;
     descriptionReserve = description;
-    var feelsLike = Math.round(parseFloat(d.main.feels_like) - 273.15);
+    let feelsLike = Math.round(parseFloat(d.main.feels_like) - 273.15);
     document.getElementById('description').innerHTML = description + '<br />' + 'feels like: ' + feelsLike + '&deg' + '<br />' + 'humidity: ' + humidity + '%' + '<br />' + 'wind: ' + wind + 'm/s';
     document.getElementById('temp').innerHTML = celsius;
     document.getElementById('location').innerHTML = d.name;
-
-    // document.getElementById('lat').textContent = lat;
-    // document.getElementById('lon').textContent = lon;
-    console.log(lat, lon);
     document.getElementById('weather-icon').innerHTML = `<img class="weather-icon" src="http://openweathermap.org/img/wn/${icon}@2x.png"/>`;
     renderItem(description);
 
@@ -106,7 +94,7 @@ window.onload = function () {
 
     forecastBalloon(625144);
 }
-//=======================================================================
+//======================================================================
 
 
 
@@ -116,28 +104,20 @@ let count = 1;
 function drawForecast(d) {
     forecastDataReserve = d;
     for (let i = 7; i < 24; i += 8) {
-        var daysForecast = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
-        // var monthsForecast = ['January','February','March','April','May','June','July','August','September','October','November','December'];
-
-        var celsius = Math.round(parseFloat(d.list[i].main.temp) - 273.15);
-        var fahrenheit = Math.round(((parseFloat(d.list[i].main.temp) - 273.15) * 1.8) + 32);
-        var icon = d.list[i].weather[0].icon;
-        var description = d.list[i].weather[0].description;
-        // var monthForecast = Number(d.list[i].dt_txt.slice(5, 7));
-        var dayForecast = Number(d.list[i].dt_txt.slice(8, 10));
-        var k = String(today.getDay()) - 1 + count;
+        let celsius = Math.round(parseFloat(d.list[i].main.temp) - 273.15);
+        let icon = d.list[i].weather[0].icon;
+        let dayForecast = Number(d.list[i].dt_txt.slice(8, 10));
+        let k = String(today.getDay()) - 1 + count;
 
         document.getElementById(`temp${count}`).innerHTML = celsius + '&deg;';
         document.getElementById(`weather-icon${count}`).innerHTML = `<img class="weather-icon1" src="http://openweathermap.org/img/wn/${icon}@2x.png"/>`;
         document.getElementById(`date${count}`).innerHTML = (k > 6 ? days[k - 7] : days[k]) + ' ' + dayForecast + '<br />';
         count++;
     }
-    //well and do something with new Date();
-
 }
 
 function forecastBalloon(cityID) {
-    var key = '29a81f0e66d7fba4e113b2cf97ccb612';
+    let key = '29a81f0e66d7fba4e113b2cf97ccb612';
     fetch('https://api.openweathermap.org/data/2.5/forecast?id=' + cityID + '&appid=' + key)
         .then(function (resp) { return resp.json() }) // Convert data to json
         .then(function (data) {
@@ -152,16 +132,16 @@ function forecastBalloon(cityID) {
 
 
 //=============================================DATE===================================================
-var today = new Date();
+let today = new Date();
 
-var days = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'];
-var months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+let days = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'];
+let months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
 
-var day = days[String(today.getDay()) - 1] || 'Sun';
-var dd = String(today.getDate()).padStart(2, '0');
-var mm = months[String(today.getMonth()).padStart(2, '0')];
+let day = days[String(today.getDay()) - 1] || 'Sun';
+let dd = String(today.getDate()).padStart(2, '0');
+let mm = months[String(today.getMonth()).padStart(2, '0')];
 
-var currentDay = day + ' ' + dd + ' ' + mm;
+let currentDay = day + ' ' + dd + ' ' + mm;
 document.getElementById('date').innerHTML = currentDay;
 //========================================================================
 
@@ -169,32 +149,27 @@ document.getElementById('date').innerHTML = currentDay;
 
 //=============================================TIME===================================================
 function updateClock() {
-    var now = new Date(),
+    let now = new Date(),
         time = now.getHours() + ':' + (now.getMinutes() < 10 ? '0' : '') + now.getMinutes();
-
-    // set the content of the element with the ID time to the formatted string
     document.getElementById('time').innerHTML = time;
-
-    // call this function again in 1000ms
     setTimeout(updateClock, 1000);
 }
 updateClock();
-//========================================================================
+//=========================================================================
 
 
 
 //=============================================BUTTONS===================================================
-let degreesValue = 'c'; //celsius or fahrenheit
 function drawDegrees() {
-    var celsius = Math.round(parseFloat(dataReserve.main.temp) - 273.15);
-    var fahrenheit = Math.round(((parseFloat(dataReserve.main.temp) - 273.15) * 1.8) + 32);
-    var feelsLikeCelsius = Math.round(parseFloat(dataReserve.main.feels_like) - 273.15);
-    var feelsLikeFahrenheit = Math.round(((parseFloat(dataReserve.main.feels_like) - 273.15) * 1.8) + 32);
+    let celsius = Math.round(parseFloat(dataReserve.main.temp) - 273.15);
+    let fahrenheit = Math.round(((parseFloat(dataReserve.main.temp) - 273.15) * 1.8) + 32);
+    let feelsLikeCelsius = Math.round(parseFloat(dataReserve.main.feels_like) - 273.15);
+    let feelsLikeFahrenheit = Math.round(((parseFloat(dataReserve.main.feels_like) - 273.15) * 1.8) + 32);
 
     if (degreesValue === 'c') {
         document.getElementById('temp').innerHTML = fahrenheit;
         for (let i = 1; i <= 3; i++) {
-            var fahrenheitFor = Math.round(((parseFloat(forecastDataReserve.list[i].main.temp) - 273.15) * 1.8) + 32);
+            let fahrenheitFor = Math.round(((parseFloat(forecastDataReserve.list[i].main.temp) - 273.15) * 1.8) + 32);
             document.getElementById(`temp${i}`).innerHTML = fahrenheitFor + '℉';
         }
         document.getElementById('degreeSign').innerHTML = '&#8457';
@@ -205,7 +180,7 @@ function drawDegrees() {
     } else {
         document.getElementById('temp').innerHTML = celsius;
         for (let i = 1; i <= 3; i++) {
-            var celsiusFor = Math.round(parseFloat(forecastDataReserve.list[i].main.temp) - 273.15);
+            let celsiusFor = Math.round(parseFloat(forecastDataReserve.list[i].main.temp) - 273.15);
             document.getElementById(`temp${i}`).innerHTML = celsiusFor + '&deg;';
         }
         document.getElementById('degreeSign').innerHTML = '°';
@@ -214,7 +189,6 @@ function drawDegrees() {
         document.getElementById('description').innerHTML = descriptionReserve + '<br />' + 'feels like: ' + feelsLikeCelsius + '&deg' + '<br />' + 'humidity: ' + humidityReserve + '%' + '<br />' + 'wind: ' + windReserve + 'm/s';
         degreesValue = 'c';
     }
-
 }
 
 document.getElementById('changeImage').addEventListener("click", function () {
@@ -225,7 +199,4 @@ document.getElementById('fahrenheit').addEventListener("click", function () {
     drawDegrees();
 }, false);
 //=========================================================================
-
-
-
 
