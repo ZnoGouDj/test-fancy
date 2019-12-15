fbbf7eb468e953d33e7d4225e2523ee92d723d6e
Egor <znogoud3@gmail.com>
2019-12-15 15:29:47 +0300

    feat: language change implement

--- index.js
+++ index.js
@@ -1,14 +1,19 @@
 let lat;
 let lon;
+let forecastDataReserve;
+let count = 1;
 let descriptionReserve;
 let dataReserve;
 let humidityReserve;
 let windReserve;
 let degreesValue = 'c'; //celsius or fahrenheit
+let feelsLikeReserve;
+let russianDescription;
+let belarusianDescription;
 
 
 
-//GEO=========================================================================
+//===============================================GEO=========================================================
 function getLocation() {
     if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(showPosition);
@@ -40,11 +45,11 @@ function showPosition(position) {
     }
     getISS();
 
-    document.getElementById('lat').textContent = lat;
-    document.getElementById('lon').textContent = lon;
+    document.getElementById('lat').textContent = 'latitude: ' + lat;
+    document.getElementById('lon').textContent = 'longitude: ' + lon;
 }
 
-//======================================================================
+//===========================================================================================================
 
 getLocation();
 //=============================================BACKGROUND===================================================
@@ -57,7 +62,7 @@ function renderItem(state = "weather") {
 
 
 
-//=============================================WEATHER===================================================
+//=============================================WEATHER======================================================
 function weatherBalloon(cityID) {
     let key = '29a81f0e66d7fba4e113b2cf97ccb612';
     fetch('https://api.openweathermap.org/data/2.5/weather?id=' + cityID + '&appid=' + key)
@@ -81,6 +86,7 @@ function drawWeather(d) {
     let description = d.weather[0].description;
     descriptionReserve = description;
     let feelsLike = Math.round(parseFloat(d.main.feels_like) - 273.15);
+    feelsLikeReserve = feelsLike;
     document.getElementById('description').innerHTML = description + '<br />' + 'feels like: ' + feelsLike + '&deg' + '<br />' + 'humidity: ' + humidity + '%' + '<br />' + 'wind: ' + wind + 'm/s';
     document.getElementById('temp').innerHTML = celsius;
     document.getElementById('location').innerHTML = d.name;
@@ -94,13 +100,11 @@ window.onload = function () {
 
     forecastBalloon(625144);
 }
-//======================================================================
+//==========================================================================================================
 
 
 
-//=============================================FORECAST===================================================
-let forecastDataReserve;
-let count = 1;
+//=============================================FORECAST=====================================================
 function drawForecast(d) {
     forecastDataReserve = d;
     for (let i = 7; i < 24; i += 8) {
@@ -127,11 +131,11 @@ function forecastBalloon(cityID) {
             // catch any errors
         });
 }
-//========================================================================
+//==========================================================================================================
 
 
 
-//=============================================DATE===================================================
+//=============================================DATE=========================================================
 let today = new Date();
 
 let days = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'];
@@ -143,11 +147,11 @@ let mm = months[String(today.getMonth()).padStart(2, '0')];
 
 let currentDay = day + ' ' + dd + ' ' + mm;
 document.getElementById('date').innerHTML = currentDay;
-//========================================================================
+//========================================================================================================
 
 
 
-//=============================================TIME===================================================
+//=============================================TIME======================================================
 function updateClock() {
     let now = new Date(),
         time = now.getHours() + ':' + (now.getMinutes() < 10 ? '0' : '') + now.getMinutes();
@@ -155,11 +159,12 @@ function updateClock() {
     setTimeout(updateClock, 1000);
 }
 updateClock();
-//=========================================================================
+//=======================================================================================================
 
 
 
 //=============================================BUTTONS===================================================
+//celsius-fahrenheit switch
 function drawDegrees() {
     let celsius = Math.round(parseFloat(dataReserve.main.temp) - 273.15);
     let fahrenheit = Math.round(((parseFloat(dataReserve.main.temp) - 273.15) * 1.8) + 32);
@@ -190,6 +195,87 @@ function drawDegrees() {
         degreesValue = 'c';
     }
 }
+//translate switch
+function translateText() {
+    let check = document.getElementById('translate').innerText;
+
+    if (check === 'ru') {
+        document.getElementById('location').innerText = 'Минск'; //DELETE ME! the other points should be easy
+
+        let date = document.getElementById('date').innerText.slice(0, 3);
+        let dateNum = document.getElementById('date').innerText.slice(4, 6);
+        let newDate = date === 'Mon' ? date = 'Пн' : date === 'Tue' ? date = 'Вт' : date === 'Wed' ? date = 'Ср' : date === 'Thu' ? date = 'Чт' : date === 'Fri' ? date = 'Пт' : date === 'Sat' ? date = 'Сб' : date = 'Вс';
+        let month = document.getElementById('date').innerText.slice(7, 10);
+        let newMonth = month === 'Jan' ? month = 'Января' : month === 'Feb' ? month = 'Февраля' : month === 'Mar' ? month = 'Марта' : month === 'Apr' ? month = 'Апреля' : month === 'May' ? month = 'Мая' : month === 'Jun' ? month = 'Июня' : month === 'Jul' ? month = 'Июля' : month === 'Aug' ? month = 'Августа' : month === 'Sep' ? month = 'Сентября' : month === 'Oct' ? month = 'Октября' : month === 'Nov' ? month = 'Ноября' : month = 'Декабря';
+        document.getElementById('date').innerText = newDate + ' ' + dateNum + ' ' + newMonth;
+        descriptionReserve === 'clear sky' ? russianDescription = 'ясно' : descriptionReserve === 'few clouds' ? russianDescription = 'слегка облачно' : descriptionReserve === 'scattered clouds' ? russianDescription = 'рассеянные облака' : descriptionReserve === 'broken clouds' ? russianDescription = 'облачно' : descriptionReserve === 'shower rain' ? russianDescription = 'ливень' : descriptionReserve === 'rain' ? russianDescription = 'дождь' : descriptionReserve === 'thunderstorm' ? russianDescription = 'гроза' : descriptionReserve === 'snow' ? russianDescription = 'снег' : russianDescription = 'туман';
+        document.getElementById('description').innerHTML = russianDescription + '<br />' + 'ощущается: ' + feelsLikeReserve + '&deg' + '<br />' + 'влажность: ' + humidityReserve + '%' + '<br />' + 'ветер: ' + windReserve + ' м/с'; //will be fail if convert in Fahrenheit but anyway
+
+        for (let i = 1; i <= 3; i++) {
+            let daysForecast = document.getElementById(`date${i}`).innerText.slice(0, 3);
+            let newForecast = daysForecast === 'Mon' ? daysForecast = 'Пн' : daysForecast === 'Tue' ? daysForecast = 'Вт' : daysForecast === 'Wed' ? daysForecast = 'Ср' : daysForecast === 'Thu' ? daysForecast = 'Чт' : daysForecast === 'Fri' ? daysForecast = 'Пт' : daysForecast === 'Sat' ? daysForecast = 'Сб' : daysForecast = 'Вс';
+            let x = document.getElementById(`date${i}`).innerText;
+            let y = x.split('').splice(4, 7).join('');
+            document.getElementById(`date${i}`).innerText = newForecast + ' ' + y;
+        }
+
+        document.getElementById('lat').innerText = 'широта: ' + lat;
+        document.getElementById('lon').innerText = 'долгота: ' + lon;
+
+        document.getElementById('translate').innerText = 'by';
+    } else if (check === 'by') {
+        document.getElementById('location').innerText = 'Мiнск'; //DELETE ME
+
+        let date = document.getElementById('date').innerText.slice(0, 2);
+
+        let dateNum = document.getElementById('date').innerText.slice(3, 5);
+        let newDate = date === 'Пн' ? date = 'Пан' : date === 'Вт' ? date = 'Аўт' : date === 'Ср' ? date = 'Сер' : date === 'Чт' ? date = 'Чац' : date === 'Пт' ? date = 'Пят' : date === 'Сб' ? date = 'Суб' : date = 'Няд';
+        let month = document.getElementById('date').innerText.slice(6, 9);
+        let newMonth = month === 'Янв' ? month = 'Студзеня' : month === 'Фев' ? month = 'Лютага' : month === 'Мар' ? month = 'Сакавiка' : month === 'Апр' ? month = 'Красавiка' : month === 'Мая' ? month = 'Мая' : month === 'Июн' ? month = 'Чэрвеня' : month === 'Июл' ? month = 'Лiпня' : month === 'Авг' ? month = 'Жнiўня' : month === 'Сен' ? month = 'Верасня' : month === 'Окт' ? month = 'Кастрычнiка' : month === 'Ноя' ? month = 'Лiстапада' : month = 'Снежня';
+        document.getElementById('date').innerText = newDate + ' ' + dateNum + ' ' + newMonth;
+
+        russianDescription === 'ясно' ? belarusianDescription = 'ясна' : russianDescription === 'слегка облачно' ? belarusianDescription = 'злёгку воблачна' : russianDescription === 'рассеянные облака' ? belarusianDescription = 'рассеяныя аблокі' : russianDescription === 'облачно' ? belarusianDescription = 'воблачна' : russianDescription === 'ливень' ? belarusianDescription = 'лівень' : russianDescription === 'дождь' ? belarusianDescription = 'дождж' : russianDescription === 'гроза' ? belarusianDescription = 'навальніца' : russianDescription === 'снег' ? belarusianDescription = 'снег' : belarusianDescription = 'туман';
+        document.getElementById('description').innerHTML = belarusianDescription + '<br />' + 'адчуваецца: ' + feelsLikeReserve + '&deg' + '<br />' + 'вільготнасць: ' + humidityReserve + '%' + '<br />' + 'вецер: ' + windReserve + ' м/с';
+
+        for (let i = 1; i <= 3; i++) {
+            let daysForecast = document.getElementById(`date${i}`).innerText.slice(0, 2);
+            let newForecast = daysForecast === 'Пн' ? daysForecast = 'Пан' : daysForecast === 'Вт' ? daysForecast = 'Аўт' : daysForecast === 'Ср' ? daysForecast = 'Сер' : daysForecast === 'Чт' ? daysForecast = 'Чац' : daysForecast === 'Пт' ? daysForecast = 'Пят' : daysForecast === 'Сб' ? daysForecast = 'Суб' : daysForecast = 'Няд';
+            let x = document.getElementById(`date${i}`).innerText;
+            let y = x.split('').splice(3, 6).join('');
+            document.getElementById(`date${i}`).innerText = newForecast + ' ' + y;
+        }
+
+        document.getElementById('lat').innerText = 'шырата: ' + lat;
+        document.getElementById('lon').innerText = 'даўгата: ' + lon;
+
+        document.getElementById('translate').innerText = 'en';
+    } else if (check === 'en') {
+        document.getElementById('location').innerText = 'Minsk'; //DELETE ME
+
+        let date = document.getElementById('date').innerText.slice(0, 3);
+        let dateNum = document.getElementById('date').innerText.slice(4, 6);
+        let newDate = date === 'Пан' ? date = 'Mon' : date === 'Аўт' ? date = 'Tue' : date === 'Сер' ? date = 'Wed' : date === 'Чац' ? date = 'Thu' : date === 'Пят' ? date = 'Fri' : date === 'Суб' ? date = 'Sat' : date = 'Sun';
+        let month = document.getElementById('date').innerText.slice(7, 10);
+        let newMonth = month === 'Студзеня' ? month = 'January' : month === 'Лютага' ? month = 'February' : month === 'Сакавiка' ? month = 'March' : month === 'Красавiка' ? month = 'April' : month === 'Мая' ? month = 'May' : month === 'Чэрвеня' ? month = 'June' : month === 'Лiпня' ? month = 'July' : month === 'Жнiўня' ? month = 'August' : month === 'Верасня' ? month = 'September' : month === 'Кастрычнiка' ? month = 'October' : month === 'Лiстапада' ? month = 'November' : month = 'December';
+        document.getElementById('date').innerText = newDate + ' ' + dateNum + ' ' + newMonth;
+
+        belarusianDescription === 'ясна' ? descriptionReserve = 'clear sky' : belarusianDescription === 'злёгку воблачна' ? descriptionReserve = 'few clouds' : belarusianDescription === 'рассеяныя аблокі' ? descriptionReserve = 'scattered clouds' : belarusianDescription === 'воблачна' ? descriptionReserve = 'broken clouds' : belarusianDescription === 'лівень' ? descriptionReserve = 'shower rain' : belarusianDescription === 'дождж' ? descriptionReserve = 'rain' : belarusianDescription === 'навальніца' ? descriptionReserve = 'thunderstorm' : belarusianDescription === 'снег' ? descriptionReserve = 'snow' : descriptionReserve = 'mist';
+        document.getElementById('description').innerHTML = descriptionReserve + '<br />' + 'feels like: ' + feelsLikeReserve + '&deg' + '<br />' + 'humidity: ' + humidityReserve + '%' + '<br />' + 'wind: ' + windReserve + ' m/s';
+
+        for (let i = 1; i <= 3; i++) {
+            let daysForecast = document.getElementById(`date${i}`).innerText.slice(0, 3);
+            let newForecast = daysForecast === 'Пан' ? daysForecast = 'Mon' : daysForecast === 'Аўт' ? daysForecast = 'Tue' : daysForecast === 'Сер' ? daysForecast = 'Wed' : daysForecast === 'Чац' ? daysForecast = 'Thu' : daysForecast === 'Пят' ? daysForecast = 'Fri' : daysForecast === 'Суб' ? daysForecast = 'Sat' : daysForecast = 'Sun';
+            let x = document.getElementById(`date${i}`).innerText;
+            let y = x.split('').splice(4, 7).join('');
+            document.getElementById(`date${i}`).innerText = newForecast + ' ' + y;
+        }
+
+        document.getElementById('lat').innerText = 'latitude: ' + lat;
+        document.getElementById('lon').innerText = 'longitude: ' + lon;
+
+        document.getElementById('translate').innerText = 'ru';
+    }
+}
 
 document.getElementById('changeImage').addEventListener("click", function () {
     renderItem(descriptionReserve);
@@ -198,5 +284,9 @@ document.getElementById('changeImage').addEventListener("click", function () {
 document.getElementById('fahrenheit').addEventListener("click", function () {
     drawDegrees();
 }, false);
-//=========================================================================
+
+document.getElementById('translate').addEventListener("click", function () {
+    translateText();
+}, false);
+//=======================================================================================================
 
